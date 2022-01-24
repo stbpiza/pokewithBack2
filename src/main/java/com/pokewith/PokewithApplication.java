a2ab06858fb64121ba5aa541182b429b7ac5d816
stbpiza <stbpiza@gmail.com>
2022-01-24 22:42:13 +0900

    초기 환경세팅 gradle 설정 application 설정 ignore 설정

---
+++ src/main/java/com/pokewith/PokewithApplication.java
@@ -0,0 +1,13 @@
+package com.pokewith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokewithApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokewithApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pokewith/PokewithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pokewith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokewithApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
