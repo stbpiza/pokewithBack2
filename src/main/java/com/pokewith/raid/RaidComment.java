5ca9178f7a3374a70c66628c61c53d33f4b55b15
stbpiza <stbpiza@gmail.com>
2022-09-08 22:37:27 +0900

    레이드 코멘트 엔티티 수정

--- src/main/java/com/pokewith/raid/RaidComment.java
+++ src/main/java/com/pokewith/raid/RaidComment.java
@@ -17,7 +17,11 @@ public class RaidComment extends TimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long raidCommentId;
 
-    private String checkNum;
+    private boolean account1;
+    private boolean account2;
+    private boolean account3;
+    private boolean account4;
+    private boolean account5;
 
 
     @ManyToOne(fetch = FetchType.LAZY)
