5b261f10ee571386943245d8871e491da8d6593f
stbpiza <stbpiza@gmail.com>
2022-10-05 00:02:55 +0900

    채팅기능 구현

--- src/main/java/com/pokewith/chat/WebSocketChatController.java
+++ src/main/java/com/pokewith/chat/WebSocketChatController.java
@@ -2,11 +2,13 @@ package com.pokewith.chat;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+@Slf4j
 @RequiredArgsConstructor
 @Component
 public class WebSocketChatController extends TextWebSocketHandler {
@@ -16,6 +18,7 @@ public class WebSocketChatController extends TextWebSocketHandler {
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        log.info("메세지 전송 = " + session + ":" + message.getPayload());
         String msg = message.getPayload();
         ChatMessage chatMessage = objectMapper.readValue(msg, ChatMessage.class);
         ChatRoom chatRoom = chatRoomRepository.findRoomById(chatMessage.getChatRoomId());
