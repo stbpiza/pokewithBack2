dce430e99a5f67da42d2d2a56308ed2d607b4419
stbpiza <stbpiza@gmail.com>
2023-03-13 00:36:26 +0900

    fix: WebSecurityConfig 수정, Repository 스캔 수정 - WebSecurityConfig 불필요한 코드 제거 - JPA, Redis Repository 스캔 범위 설정

--- src/main/java/com/pokewith/PokewithApplication.java
+++ src/main/java/com/pokewith/PokewithApplication.java
@@ -2,7 +2,9 @@ package com.pokewith;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories(basePackages = {"com.pokewith.user.repository", "com.pokewith.raid.repository"})
 @SpringBootApplication
 public class PokewithApplication {
 
